654b3b10ef496d746e174fef3f36e2cb345e9d18
Moon Dahyun <dahyun723@ajou.ac.kr>
2023-10-20 18:37:09 +0900

    feat:script.js추가

---
+++ 1주차/4.가계부/script.js
@@ -0,0 +1,25 @@
+const INIT_BALANCE = 0;
+const HISTORY_LIST = {
+    0: {
+        category : "식비",
+        main : '버거킹',
+        price : 10800
+    },
+    1: {
+        category : "식비2",
+        main : '버거킹',
+        price : 10800
+    },
+    2: {
+        category : "식비3",
+        main : '버거킹',
+        price : 10800
+    },
+    3: {
+        category : "식비4",
+        main : '버거킹',
+        price : 10800
+    },
+}
+
+let balance = document.querySelectorAll('category'); 
