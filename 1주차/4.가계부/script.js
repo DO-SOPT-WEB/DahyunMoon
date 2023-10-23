225ffe71d25a92b498e78d6e882cdc78fd5661e2
Moon Dahyun <dahyun723@ajou.ac.kr>
2023-10-24 02:12:05 +0900

    Feat: 수입/지출 필터링

--- 1주차/4.가계부/script.js
+++ 1주차/4.가계부/script.js
@@ -6,43 +6,80 @@ const HISTORY_LIST = [
   { category: "입금", content: "꼰떼넨떼더광교", price: 277056 },
 ];
 
-const list_real = document.querySelector(".list-real");
-const income = document.querySelector(".income");
-const outcome = document.querySelector(".outcome");
-const total = document.querySelector(".total");
+const incomeButton = document.querySelector(
+  "input[type=checkbox][value='income']"
+);
+const outcomeButton = document.querySelector(
+  "input[type=checkbox][value='outcome']"
+);
 
-let total_income = 0;
-let total_outcome = 0;
+const initial_list = document.querySelector(".list-real");
 
-HISTORY_LIST.forEach((item) => {
-  const ul = document.createElement("ul");
-  ul.classList.add("list-indiv");
+function filter_history() {
+  const show_income = incomeButton.checked;
+  const show_outcome = outcomeButton.checked;
 
-  const category_li = document.createElement("li");
-  category_li.classList.add("category");
-  category_li.textContent = item.category;
+  const filtered_list = filter_item(show_income, show_outcome);
+  show_list(filtered_list);
+}
 
-  const content_li = document.createElement("li");
-  content_li.classList.add("content");
-  content_li.textContent = item.content;
+function filter_item(show_income, show_outcome) {
+  return HISTORY_LIST.filter((item) => {
+    //return 없으면 반환안함
+    if (show_income && show_outcome) {
+      return true;
+    }
+    if (show_income) return item.price > 0;
+    if (show_outcome) return item.price < 0;
+    return false;
+  });
+}
 
-  const price_li = document.createElement("li");
-  price_li.classList.add("price");
-  price_li.textContent =
-    item.price < 0 ? `${item.price}원` : `+${item.price}원`;
-  if (item.price < 0) {
-    price_li.style.color = "blue";
-    total_outcome += item.price;
-  } else {
-    price_li.style.color = "red";
-    total_income += item.price;
-  }
+incomeButton.addEventListener("change", filter_history);
+outcomeButton.addEventListener("change", filter_history);
 
-  list_real.appendChild(ul);
-  ul.appendChild(category_li);
-  ul.appendChild(content_li);
-  ul.appendChild(price_li);
-});
-total.textContent = `${total_income - total_outcome}원`;
-income.textContent = `+${total_income}`;
-outcome.textContent = `${total_outcome}`;
+function show_list(history_list) {
+  initial_list.innerHTML = ""; // 이전 내역 지우기
+  const list_real = document.querySelector(".list-real");
+  const income = document.querySelector(".income");
+  const outcome = document.querySelector(".outcome");
+  const total = document.querySelector(".total");
+
+  let total_income = 0;
+  let total_outcome = 0;
+
+  history_list.forEach((item) => {
+    const ul = document.createElement("ul");
+    ul.classList.add("list-indiv");
+
+    const category_li = document.createElement("li");
+    category_li.classList.add("category");
+    category_li.textContent = item.category;
+
+    const content_li = document.createElement("li");
+    content_li.classList.add("content");
+    content_li.textContent = item.content;
+
+    const price_li = document.createElement("li");
+    price_li.classList.add("price");
+    price_li.textContent =
+      item.price < 0 ? `${item.price}원` : `+${item.price}원`;
+    if (item.price < 0) {
+      price_li.style.color = "blue";
+      total_outcome += item.price;
+    } else {
+      price_li.style.color = "red";
+      total_income += item.price;
+    }
+
+    list_real.appendChild(ul);
+    ul.appendChild(category_li);
+    ul.appendChild(content_li);
+    ul.appendChild(price_li);
+  });
+  total.textContent = `${total_income - total_outcome}원`;
+  income.textContent = `+${total_income}`;
+  outcome.textContent = `${total_outcome}`;
+}
+
+filter_history();
