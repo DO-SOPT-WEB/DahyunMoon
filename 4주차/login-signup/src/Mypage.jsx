0d25d51907eac5e02f58ee63a021d823dfc74143
Moon Dahyun <dahyun723@ajou.ac.kr>
2023-11-17 03:11:05 +0900

    feat: myPage에 정보 띄우기

--- 4주차/login-signup/src/Mypage.jsx
+++ 4주차/login-signup/src/Mypage.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import styled from "styled-components";
 import "./App.css";
 import axios from "axios";
@@ -6,8 +6,31 @@ import { useParams } from "react-router-dom";
 
 export default function MyPage() {
   let { userId } = useParams();
+  const [username, setUsername] = useState("");
+  const [nickname, setNickname] = useState("");
   console.log(userId);
-  return <MainBox>{userId}</MainBox>;
+  useEffect(() => {
+    const fetchUserInfo = async () => {
+      try {
+        const response = await axios.get(
+          import.meta.env.VITE_BASE_URL + `/api/v1/members/${userId}`
+        );
+        console.log("response-data", response.data);
+        setUsername(response.data.username);
+        setNickname(response.data.nickname);
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+    fetchUserInfo();
+  });
+  return (
+    <MainBox>
+      <Header>MY PAGE</Header>
+      <Info>ID : {username}</Info>
+      <Info>NICKNAME : {nickname}</Info>
+    </MainBox>
+  );
 }
 
 const MainBox = styled.main`
@@ -19,4 +42,19 @@ const MainBox = styled.main`
   position: relative;
 
   padding: 1rem 2rem;
+`;
+
+const Header = styled.header`
+  font-size: 2rem;
+  font-weight: bold;
+
+  margin: 1rem auto;
+`;
+
+const Info = styled.div`
+  font-weight: bold;
+  height: 3rem;
+  padding: 0 1rem;
+
+  line-height: 3rem;
 `;
