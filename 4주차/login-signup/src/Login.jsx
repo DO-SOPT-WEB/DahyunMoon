860e1627a8be24697a6e4d37a8912bb0c834d5ea
Moon Dahyun <dahyun723@ajou.ac.kr>
2023-11-17 15:47:04 +0900

    feat: createPortal로 toast

--- 4주차/login-signup/src/Login.jsx
+++ 4주차/login-signup/src/Login.jsx
@@ -1,12 +1,16 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import styled from "styled-components";
 import "./App.css";
 import axios from "axios";
 import { Link, Navigate, useNavigate } from "react-router-dom";
+import { createPortal } from "react-dom";
 
 export default function Login() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+
+  const [showToast, setShowToast] = useState(false);
+  const [toastMessage, setToastMessage] = useState("");
 
   const navigate = useNavigate();
 
@@ -30,9 +34,25 @@ export default function Login() {
         console.log(response.data.id);
       })
       .catch((error) => {
+        setToastMessage(error.response.data.message);
+        setShowToast(true);
         console.log("An error occurred:", error.response);
       });
   };
+
+  useEffect(() => {
+    // 3초 후 사라짐
+    const timer = setTimeout(() => {
+      setShowToast(false);
+    }, 3000);
+
+    return () => clearTimeout(timer);
+  }, [showToast]);
+
+  const toastElement = showToast ? (
+    <ToastContainer>{toastMessage}</ToastContainer>
+  ) : null;
+
   return (
     <MainBox>
       <Header>Login</Header>
@@ -66,6 +86,7 @@ export default function Login() {
       <Link to="/sign-up">
         <Button>회원가입</Button>
       </Link>
+      {createPortal(toastElement, document.body)}
     </MainBox>
   );
 }
@@ -127,4 +148,15 @@ const Button = styled.button`
 
   margin: 0.5rem auto;
   border: 1px solid black;
+`;
+
+const ToastContainer = styled.div`
+  position: fixed;
+  bottom: 5rem;
+  left: 50%;
+  transform: translateX(-50%);
+  background-color: gray;
+  color: white;
+  padding: 1rem;
+  border-radius: 0.5rem;
 `;
