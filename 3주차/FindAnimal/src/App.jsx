b819248361974c10b65eaa4eefef09fc277fc902
Moon Dahyun <dahyun723@ajou.ac.kr>
2023-12-07 01:09:49 +0900

    refactor : page4 분리

--- 3주차/FindAnimal/src/App.jsx
+++ 3주차/FindAnimal/src/App.jsx
@@ -7,6 +7,7 @@ import Page0 from "./pages/page0";
 import Page1 from "./pages/Page1";
 import Page2 from "./pages/Page2";
 import Page3 from "./pages/Page3";
+import Page4 from "./pages/Page4";
 
 function App() {
   const [showPage0, setShowPage0] = useState(true);
@@ -166,63 +167,13 @@ function PageRandom({ animalData }) {
   );
 }
 
-function Page4({ selectedOptions, animalData, restartPage1 }) {
-  function getMatchingAnimal(selectedOptions) {
-    const matchingAnimalData = animalData.find(
-      (data) =>
-        data.answer.length === selectedOptions.length &&
-        data.answer.toString() === selectedOptions.toString()
-    );
-
-    return matchingAnimalData ? matchingAnimalData : "알 수 없음";
-  }
-  const handleRestart = () => {
-    restartPage1();
-  };
-  const matchingAnimal = getMatchingAnimal(selectedOptions).animal;
-  const matchingAnimalPic = getMatchingAnimal(selectedOptions).imgUrl;
-
-  return (
-    <div>
-      <Group>
-        <TextBox>너랑 닮은 동물은.. {matchingAnimal}(이)야!!</TextBox>
-        <AnimalPic src={matchingAnimalPic}></AnimalPic>
-        <div>
-          <ClickButton onClick={handleRestart}>다시하기</ClickButton>
-        </div>
-      </Group>
-    </div>
-  );
-}
-
 export default App;
-
-const Question = styled.button`
-  width: 15rem;
-  height: 15rem;
-  border-radius: 3rem;
-  margin: 3rem 3rem;
-  font-size: 1.5rem;
-  font-weight: bold;
-  &:hover {
-    background-color: #6495ed;
-  }
-`;
 
 const Group = styled.div`
   background-color: #d1eaf0;
   width: 80vw;
   height: 60vh;
   border-radius: 0.5rem;
-`;
-
-const ClickButton = styled.button`
-  background-color: #6495ed;
-  color: white;
-  margin: 0 1rem;
-  width: 8rem;
-  height: 3rem;
-  font-weight: bold;
 `;
 
 const NavigateButton = styled.button`
---
+++ 3주차/FindAnimal/src/pages/Page4.jsx
@@ -0,0 +1,74 @@
+import { useState } from "react";
+import Header from "../components/Header";
+import styled from "styled-components";
+import AnswerButton from "../components/AnswerButton";
+
+function Page4({ selectedOptions, animalData, restartPage1 }) {
+  function getMatchingAnimal(selectedOptions) {
+    const matchingAnimalData = animalData.find(
+      (data) =>
+        data.answer.length === selectedOptions.length &&
+        data.answer.toString() === selectedOptions.toString()
+    );
+
+    return matchingAnimalData ? matchingAnimalData : "알 수 없음";
+  }
+  const handleRestart = () => {
+    restartPage1();
+  };
+  const matchingAnimal = getMatchingAnimal(selectedOptions).animal;
+  const matchingAnimalPic = getMatchingAnimal(selectedOptions).imgUrl;
+
+  return (
+    <div>
+      <Group>
+        <TextBox>너랑 닮은 동물은.. {matchingAnimal}(이)야!!</TextBox>
+        <AnimalPic src={matchingAnimalPic}></AnimalPic>
+        <div>
+          <ClickButton onClick={handleRestart}>다시하기</ClickButton>
+        </div>
+      </Group>
+    </div>
+  );
+}
+
+export default Page4;
+
+const Group = styled.div`
+  background-color: #d1eaf0;
+  width: 80vw;
+  height: 60vh;
+  border-radius: 0.5rem;
+`;
+
+const TextBox = styled.div`
+  font-size: 1.5rem;
+  font-weight: bold;
+  margin: 0.5rem 0;
+`;
+
+const NavigateButton = styled.button`
+  background-color: white;
+  &:hover {
+    background-color: #6495ed;
+  }
+  &:disabled {
+    background-color: gray;
+  }
+`;
+
+const AnimalPic = styled.img`
+  margin-top: 1rem;
+  width: 70%;
+  height: 70%;
+  object-fit: cover;
+`;
+
+const ClickButton = styled.button`
+  background-color: #6495ed;
+  color: white;
+  margin: 0 1rem;
+  width: 8rem;
+  height: 3rem;
+  font-weight: bold;
+`;
